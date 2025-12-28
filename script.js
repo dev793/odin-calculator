6f70d7dce279a2d0e00a74d8cdd02accc0fea8f2
dev793 <117709900+dev793@users.noreply.github.com>
2025-12-29 10:44:14 +1300

    Create operate function and data variables

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
+
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+
+
+//expression = operand1 + operator + operation2
